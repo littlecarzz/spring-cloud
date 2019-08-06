8b11d806be8bf0ca7b811e494364e2b0f87b3aaf
littlecar <xc_chejj@163.com>
2019-08-06 17:02:23 +0800

    first commit

---
+++ spring-cloud-eureka-cluster/src/main/java/com/cjj/SpringCloudEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.cjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 描述：
+ *SpringCloudEurekaApplication 启动类
+ * @author littlecar
+ * @date 2019/8/6 14:47
+ */
+@SpringBootApplication
+//启动eureka server
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+    public static void main(String[] arg) {
+        SpringApplication.run(SpringCloudEurekaApplication.class);
+    }
+}
